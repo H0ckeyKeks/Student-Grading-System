86b4b867ba5fcc23c60b3125086d82f17a556eaf
Jasmin <jasmin_suedhoff@web.de>
2025-01-18 12:26:45 +0100

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Student_Grading_System
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Creating a list and adding students
+            List<Student> students = new List<Student>()
+            {
+                new Student("Alice", 1),
+                new Student("Jack", 2),
+                new Student("Mike", 3),
+                new Student("Jane", 4),
+            };
+
+            // Adding grades
+            students[0].AddGrade(85);
+            students[0].AddGrade(92);
+            students[1].AddGrade(95);
+            students[1].AddGrade(97);
+            students[2].AddGrade(50);
+            students[2].AddGrade(65);
+            students[3].AddGrade(25);
+            students[3].AddGrade(10);
+
+            // sorting the students by average grade
+            var sortedStudents = students.OrderByDescending(x => x.CalculateAverageGrade());
+
+            Console.WriteLine("\nSorted students: ");
+            foreach (var student in sortedStudents)
+            {
+                Console.WriteLine(student.PrintOutput());
+            }
+
+            // filtering out failed students (average grade below 50)
+            var failedStudents = students.Where(x => x.CalculateAverageGrade() < 50);
+
+            Console.WriteLine("\nFailed students: ");
+            foreach (var student in failedStudents)
+            {
+                Console.WriteLine(student.PrintOutput());
+            }
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_Grading_System
+{
+    internal class Student
+    {
+        private string _name;
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        private int _rollNo;
+        public int RollNo
+        {
+            get { return _rollNo; }
+            set { _rollNo = value; }
+        }
+
+        private List<double> _grades;
+
+        public List<double> Grades
+        {
+            get { return _grades; }
+            set { _grades = value; }
+        }
+
+        public Student(string name, int rollNo)
+        {
+            Name = name;
+            RollNo = rollNo;
+            Grades = new List<double>();
+        }
+
+        public void AddGrade(double grade)
+        {
+            Grades.Add(grade);
+        }
+
+        public double CalculateAverageGrade()
+        {
+            if (Grades.Count == 0)
+            {
+                return 0;
+            }
+            
+            double sumGrades = 0;
+
+            foreach (double item in Grades)
+            {
+                sumGrades += item;
+            }
+
+            double average = sumGrades / Grades.Count;
+
+            return average;
+        }
+
+        public double GetHighestGrade()
+        {
+            if (Grades.Count == 0)
+            {
+                return 0;
+            }
+            
+            double highest = 0;
+
+            foreach (double item in Grades)
+            {
+                if (item > highest)
+                {
+                    highest = item;
+                }
+            }
+
+            return highest;
+        }
+
+        public double GetLowestGrade()
+        {
+            if (Grades.Count == 0)
+            {
+                return 0;
+            }
+            
+            double lowest = 0;
+
+            foreach (double item in Grades)
+            {
+                if (item < lowest)
+                {
+                    lowest = item;
+                }
+            }
+
+            return lowest;
+        }
+
+        public string PrintOutput()
+        {
+            return String.Format($"Name: {Name}, RollNo: {RollNo}, Average Grade: {CalculateAverageGrade()}");
+        }
+    }
+}
